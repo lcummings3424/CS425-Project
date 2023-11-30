dc915e0545ba148f39cbeb31cdeab3d6c4a93ef9
NikoHatzisavas724 <nikohatzisavas@outlook.com>
2023-11-30 04:23:33 -0600

    update sample data

--- Sample_data.sql
+++ Sample_data.sql
@@ -1,2 +1,2 @@
 INSERT INTO users(user_id, first_name,middle_name,last_name ,email ,hashed_password ,phone)
-VALUES(1, 'john', 'middle','smith','js@gmail.com',2,2)
+VALUES(1, 'john', 'middle','smith','js@gmail.com','asdhdsak13231213',2245555555)
