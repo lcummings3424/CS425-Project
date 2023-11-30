12ad6efdc91e887a9b409606aec45865e1c09281
NikoHatzisavas724 <nikohatzisavas@outlook.com>
2023-11-30 15:15:34 -0600

    updated joined time

--- Schema.sql
+++ Schema.sql
@@ -8,7 +8,7 @@ CREATE TABLE
         email TEXT UNIQUE NOT NULL,
         hashed_password TEXT NOT NULL,
         join_date DATE DEFAULT CURRENT_DATE,
-        joined_time INTEGER,
+        joined_time TIME DEFAULT CURRENT_TIME,
         phone BIGINT UNIQUE
     );
 
