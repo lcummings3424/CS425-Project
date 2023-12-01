8cbdaf01b00fc17e24a410cfa8ec6713abf402ef
NikoHatzisavas724 <nikohatzisavas@outlook.com>
2023-11-30 22:48:49 -0600

    added more sample data

--- Sample_data.sql
+++ Sample_data.sql
@@ -17,4 +17,22 @@ VALUES
         'js@gmail.com',
         'asdhdsak13231213',
         2245555555
+    ),
+    (
+        2,
+        'jeff',
+        'mid',
+        'brown',
+        'jb@gmail.com',
+        'dsadas54145',
+        2245555556
+    ),
+    (
+        3,
+        'jared',
+        'm',
+        'leto',
+        'jl@gmail.com',
+        'fgfsd451',
+        2245555557
     )
