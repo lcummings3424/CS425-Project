145ee1315b423f78649246ff36095725b79ec202
NikoHatzisavas724 <nikohatzisavas@outlook.com>
2023-12-03 22:56:13 -0600

    added python script for commiting sample data

---
+++ add_sample_data.py
@@ -0,0 +1,14 @@
+import psycopg2
+import tkinter as tk
+
+con = psycopg2.connect(
+    database="test",
+    user="postgres",
+    password="postgres",
+    host="localhost",
+    port="5432",
+)
+cursor_obj = con.cursor()
+sql_file = open("Sample_data.sql", "r")
+cursor_obj.execute(sql_file.read())
+con.commit()
